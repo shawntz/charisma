3a8cbd2abe65eeed963be997f0ad99b9167f6472
Shawn Schwartz <shawnschwartz@ucla.edu>
2020-01-26 11:20:43 -0800

    fixed analyzing console output format

--- autocolor-testscript.R
+++ autocolor-testscript.R
@@ -12,5 +12,5 @@ k_out <- run_all_ks("testing",1,3,method = "elbow",psi=2, visualize = 1)
 k_out
 
 
-color_fishes_out_new <- run_all_ks("c",1,4,method="elbow",visualize = 1, fileout = "newtest.txt")
+color_fishes_out_new <- run_all_ks("colors",1,4,method="elbow",visualize = 1, fileout = "newtest.txt")
 color_fishes_out_new
