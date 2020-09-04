0b7ec9aa125ad18394d3014e8fa5ecdbe95c6574
Shawn Schwartz <shawnschwartz@ucla.edu>
2020-09-04 00:19:37 -0700

    update demo script and readme accordingly

--- charisma-demo-script.R
+++ charisma-demo-script.R
@@ -6,8 +6,7 @@
 #### Setup ####
 rm(list = ls())
 
-#wd <- "~/Dropbox/Research/UCLA/Alfaro-Lab/charisma_paper/charisma"
-wd <- "C:/Users/shawn/Downloads/charisma"
+wd <- "C:/Users/shawn/charisma"
 setwd(wd)
 
 #### Load charisma Source ####
@@ -23,30 +22,34 @@ images_path <- file.path("demo", "tanagers")
 # (e.g, if background is solid green (i.e., R=0,G=1.0,B=0), then lowerG and upperG should be set to 1.0
 # to ignore only the solid green parts of the image (i.e., the masked out background pixels))
 lowerR <- 0.0
-lowerG <- 1.0
+lowerG <- 0.55
 lowerB <- 0.0
-upperR <- 0.0
+upperR <- 0.24
 upperG <- 1.0
-upperB <- 0.0
+upperB <- 0.24
 thresh <- .05 # (5% minimum threshold)
-method <- "GE" # greater than or equal to method (contrasted to "G" ~> greater than only for threshold comparison)
+method <- "GE" # greater than or equal to method (contrasted to 'G' ~> greater than only for threshold comparison)
+mode <- "lower" # set to either 'lower' or 'upper'
 colorspace <- "rgb" # set to either 'rgb' or 'hsv'
+width <- 750 # pixel width of output diagnostic plots
+height <- 500 # pixel height of output diagnostic plots
+colorwheel <- TRUE
 
 #### Run Pipeline ####
 #This step uses the masked versions of the images
 k_out <- autoComputeKPipeline(images_masked_path, diagnosticMode = TRUE,
                               lowerR = lowerR, lowerG = lowerG, lowerB = lowerB,
                               upperR = upperR, upperG = upperG, upperB = upperB, 
-                              colOut = TRUE, thresh = thresh, method = method, saveDiagnosticPlots = TRUE,
-                              colorspace = colorspace)
+                              colOut = TRUE, mode = mode, thresh = thresh, method = method, saveDiagnosticPlots = TRUE,
+                              width = width, height = height, colorspace = colorspace, colorwheel = colorwheel)
 
-write.csv(k_out, "tanagers_k_values_output.csv")
+write.csv(k_out, "tanagers_k_values_output.csv", row.names = FALSE)
 
 #### Run Color Pattern Analysis ####
 #This step uses the unmasked versions of the images
 color_classified <- classifyColorPipeline(images_path, k_out)
 color_classified
-write.csv(color_classified, "color_pattern_analysis_tanagers_demo.csv")
+write.csv(color_classified, "color_pattern_analysis_tanagers_demo.csv", row.names = FALSE)
 
 pca_k <- runColorPCA(color_classified)
 pca_k
