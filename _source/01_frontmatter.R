fd406f2f60156da708257012008ee43a7ea95474
Shawn Schwartz <shawnschwartz@ucla.edu>
2021-04-06 16:04:28 -0700

    transfer old code to brainstorming - clean master branch

--- _source/01_frontmatter.R
+++
@@ -1,6 +0,0 @@
-cat("\n\n    #######################################################################
-    ##                              charisma                             ##
-    ##  Automatic Detection of Color Classes for Color Pattern Analysis  ##
-    ##                  Written by Shawn Schwartz, 2020                  ##
-    ##                      <shawnschwartz@ucla.edu>                     ##
-    #######################################################################\n\n\n")
--- charisma.source.compile.R
+++
@@ -1,5 +0,0 @@
-#### Compile Source Modules ----
-for(f in list.files("_source", pattern = "*.R")) 
-{
-  source(paste0("_source", "/", f))
-}
