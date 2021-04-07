62ae735d39e2df5b534994cac0f302670a67a323
Shawn Schwartz <shawnschwartz@ucla.edu>
2021-04-07 05:53:13 -0700

    add compiler code

--- compile.R
+++ compile.R
@@ -1,4 +1,30 @@
-#### Compile Source Modules ----
+#### Launch Frontmatter ####
+cat("\n\n    #######################################################################
+    ##                              Charisma                             ##
+    ##  Automatic Detection of Color Classes for Color Pattern Analysis  ##
+    ##                      <shawnschwartz@ucla.edu>                     ##
+    #######################################################################\n\n\n")
+
+#### Begin Initialization ####
+cat("\n    Initializing Charisma...\n")
+options(warn=-1)
+
+#### Install Required Libraries ####
+required_libraries <- c("tidyverse", "plyr", "magick", "gridExtra")
+if(length(setdiff(required_libraries, rownames(installed.packages()))) > 0) {
+  install.packages(setdiff(required_libraries, rownames(installed.packages())),
+                   repos = "https://ftp.osuosl.org/pub/cran/") #set CRAN mirror
+}
+
+#### Load Required Libraries ####
+for(libs in required_libraries) {
+  eval(bquote(suppressMessages(library(.(libs)))))
+}
+
+#### Finish Initialization ####
+cat("\n    Finished loading Charisma!\n\n")
+
+#### Compile Source Modules ####
 for(f in list.files("_source/R", pattern = "*.R")) 
 {
   source(paste0("_source/R", "/", f))
