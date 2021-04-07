1432ff4ce6e5bb2d1fcea302016897b3f2fc7215
Shawn Schwartz <shawnschwartz@ucla.edu>
2021-04-07 05:41:42 -0700

    fix mapping validation return logic

--- _source/R/validateMapping.R
+++ _source/R/validateMapping.R
@@ -10,19 +10,22 @@ validateMapping <- function(mapping) {
   
   ##validate along each slice
   missing <- list()
+  total_n_missing <- 0
   for(ii in 1:length(slices)) {
     slice <- readImg(slices[ii], alpha = F)
     calls <- callColors(slice, mapping)
-    n_missing <- nrow(slice) - sum(calls$total)
     
-    if(n_missing > 0) {
-      missing[[ii]] <- calls[calls$total == 0,]
-      cat("Missing color calls. Mapping validation failed. See output for missing pixels.\n")
-      return(missing)
-    } else {
-      cat("No missing color calls. Mapping successfully validated!\n")
-      return(TRUE)
-    }
+    n_missing <- nrow(slice) - sum(calls$total)
+    total_n_missing <- total_n_missing + n_missing
+    missing[[ii]] <- calls[calls$total == 0,]
+  }
+  
+  if(total_n_missing > 0) {
+    cat(paste0("Missing color calls for ", total_n_missing, " pixels ==> Mapping validation failed.\nSee output for missing pixels.\n"))
+    return(missing)
+  } else {
+    cat("No missing color calls. Mapping successfully validated!\n")
+    return(TRUE)
   }
   
 }
