bf260630ea1b433cdc4e41207c5b355c64ed80c8
Shawn Schwartz <shawnschwartz@ucla.edu>
2021-04-09 16:14:39 -0700

    add main plotting function

--- _source/R/readImg.R
+++ _source/R/readImg.R
@@ -1,8 +1,18 @@
 readImg <- function(path, alpha = T) {
   
   img <- loadImg(path, lower = NULL, upper = NULL, alpha.channel = alpha)
+  
+  ##save original image dimensions for later plotting
+  nrows <- dim(img$original.rgb)[1]
+  ncols <- dim(img$original.rgb)[2]
+  
+  ##refactor img as dataframe in long format
+  rgb_version <- img$filtered.rgb.2d
   img <- as.data.frame(img$hsv.version)
+  img <- cbind(img, rgb_version)
   img$id <- seq(1, nrow(img))
+  img$nrows <- rep(nrows, nrow(img))
+  img$ncols <- rep(ncols, nrow(img))
   
   return(img)
   
---
+++ _source/R/rgb2hex.R
@@ -0,0 +1,5 @@
+rgb2hex <- function(r, g, b) {
+  
+  return(rgb(r, g, b, maxColorValue = 1))
+  
+}
