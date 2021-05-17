38492a61bdf6b00c16843f6541b2485e1ca148fd
Shawn Schwartz <shawnschwartz@ucla.edu>
2021-05-17 10:16:00 -0700

    add crosshair plots for centroids -- underlying algorithm needs to be fixed

--- _source/R/averageCentroidDistance_TODO.R
+++ _source/R/averageCentroidDistance_TODO.R
@@ -1,17 +1,40 @@
-library(tidyverse)
-​
-#pass in csv with x and y coordinates and the specified color as string 
-# example: averageCentroidDistance("demo_bird_with_xy_coords.csv", "purple")
-#function is slow reading in csv each call. Could change it to read it in prior and then use the dataframe inside
-#color would be the only argument
-​
+
 ## TODO: SHAWN:
   ## create verbose output with x,y calculated centroids 
   ## need to add in filter out is.bg column
   ## make white halo around color in sprite plot matrix using rounded centroid coordinate
 
-averageCentroidDistance <- function(df, color) {
+getCentroidCoordinateX <- function(color, df) {
+  #filter out background pixels
+  df <- subset(df, is.bg == 0)  
+  
+  #filter by color
+  df <- df[ df[[color]] == 1, ]
+  
+  #coordinates of the centroid
+  meanx <- sum(df$x.coord) / nrow(df)
+  
+  return(ceiling(meanx))
+}
+
+getCentroidCoordinateY <- function(color, df) {
+  #filter out background pixels
+  df <- subset(df, is.bg == 0) 
+  
+  #filter by color
+  df <- df[ df[[color]] == 1, ]
+  
+  #coordinates of the centroid
+  meany <- sum(df$y.coord) / nrow(df)
+  
+  return(ceiling(meany))
+}
+
+getAverageCentroidDistance <- function(color, df) {
   #df <- read.csv(parsed_birds_csv)
+  
+  #filter out background pixels
+  df <- subset(df, is.bg == 0) 
   
   #filter by color
   df <- df[ df[[color]] == 1, ]
@@ -19,8 +42,6 @@ averageCentroidDistance <- function(df, color) {
   #x and y coordinates of the centroid 
   meanx <- sum(df$x.coord) / nrow(df)
   meany <- sum(df$y.coord) / nrow(df)
-  
-  print(paste0(meanx, ", ", meany))
   
   #define centroid distance calculation for apply
   calculateCentroidDist <- function(df) {
@@ -33,5 +54,5 @@ averageCentroidDistance <- function(df, color) {
   centroidDist <- apply(df, 1, calculateCentroidDist)
   avgCentroidDistance <- sum(centroidDist) / nrow(df)
   
-  return(avgCentroidDistance)
+  return(log(avgCentroidDistance))
 }
--- _source/R/plotSprite.R
+++ _source/R/plotSprite.R
@@ -11,7 +11,41 @@ plotSprite <- function(img, mapping) {
     }
   }
   
+  ##get all color names from mapping
+  colors <- getMappedColors(mapping)
+  
   hex_values <- getHexValuesMatrix(img, mapping)
+  
+  ##modify hex to have centroid halos
+  x_coords <- sapply(colors, getCentroidCoordinateX, img, simplify = T)
+  y_coords <- sapply(colors, getCentroidCoordinateY, img, simplify = T)
+  centroid_distances <- sapply(colors, getAverageCentroidDistance, img, simplify = T)
+  
+  print(x_coords)
+  print(y_coords)
+  
+  halo_color <- "#FF00D9"
+  
+  ##make hex vector
+  hex_vector <- getHexVector(mapping)
+  
+  for(ii in 1:length(centroid_distances)) {
+    for(jj in 1:100) {
+      for(kk in 1:15) {
+        ##up
+        hex_values[x_coords[ii]+kk, y_coords[ii]+jj+kk] <- hex_vector[ii]
+        
+        ##down
+        hex_values[x_coords[ii]+kk, y_coords[ii]-jj+kk] <- hex_vector[ii]
+        
+        ##left
+        hex_values[x_coords[ii]-jj+kk, y_coords[ii]+kk] <- hex_vector[ii]
+        
+        ##right
+        hex_values[x_coords[ii]+jj+kk, y_coords[ii]+kk] <- hex_vector[ii]
+      }
+    }
+  }
   
   asp <- img$nrows[1] / img$ncols[1]
   
