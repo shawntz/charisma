cd75b182d03b3fa87bffb80aac9e92b2e38b7692
Shawn Schwartz <shawnschwartz@ucla.edu>
2021-05-18 03:42:10 -0700

    add getImagePaths function for pipeline from colordistance package

---
+++ R/getImagePaths.R
@@ -0,0 +1,30 @@
+## Copied from R::colordistance function `getImagePaths`
+getImagePaths <- function(path) {
+
+  # Make sure input is both a string and a valid folder path
+  if (!is.character(path)) {
+
+    stop("Provided filepath is not a string (character type)")
+
+  } else if (!file.exists(path)) {
+
+    stop("Folder does not exist")
+
+  } else {
+
+    im.dir <- normalizePath(dir(path, pattern = "^.*.(jpg|jpeg|png)$",
+                                ignore.case = T, full.names = T))
+    # returns absolute filepaths of images in folder that end in either .jpg,
+    # .jpeg, or .png (case-insensitive)
+
+    # If no images were found but the folder path was valid, print message
+    # instead of returning an empty vector; otherwise return vector of image
+    # paths
+    if (length(im.dir) == 0) {
+      message(paste("No images of compatible format (JPG or PNG) in", path))
+    } else {
+      return(im.dir)
+    }
+  }
+
+}
