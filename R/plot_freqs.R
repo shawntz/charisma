bb24fa90823ac6690e6889926b00596bd5d7bef1
Shawn Schwartz <shawn.t.schwartz@gmail.com>
2024-02-05 17:29:40 -0800

    fix bug #9

--- R/plot_freqs.R
+++ R/plot_freqs.R
@@ -1,8 +1,22 @@
 plot_freqs <- function(charisma_obj, use.default.bar.colors = TRUE) {
-  hex <- get_mapped_hex()
-  color_summary <- hex %>% rename(classification = color.name) %>% left_join(charisma_obj$charisma_calls_table, by = "classification")
+  if (length(charisma_obj$dropped_colors) > 0) {
+    color_table <- charisma_obj$charisma_calls_table_no_threshold
+  } else {
+    color_table <- charisma_obj$charisma_calls_table
+  }
 
-  cluster_specific_hex_vals <- charisma_obj$color_mask_LUT %>% group_by(classification, hex) %>% summarise(mean_prop = mean(prop)) %>% select(-mean_prop) %>% rename(new.hex = hex) %>% right_join(color_summary, by = "classification") %>% arrange(classification)
+  hex <- get_mapped_hex()
+  color_summary <- hex %>%
+    rename(classification = color.name) %>%
+    left_join(color_table, by = "classification")
+
+  cluster_specific_hex_vals <- charisma_obj$color_mask_LUT %>%
+    group_by(classification, hex) %>%
+    summarise(mean_prop = mean(prop)) %>%
+    select(-mean_prop) %>%
+    rename(new.hex = hex) %>%
+    right_join(color_summary, by = "classification") %>%
+    arrange(classification)
 
   # freq_bar <- barplot(height = color_summary$prop, names = color_summary$classification, col = color_summary$default.hex,
 
@@ -12,7 +26,9 @@ plot_freqs <- function(charisma_obj, use.default.bar.colors = TRUE) {
     bar_colors = cluster_specific_hex_vals$new.hex
   }
 
-  freq_bar <- barplot(height = cluster_specific_hex_vals$prop, names = cluster_specific_hex_vals$classification, col = bar_colors,
+  freq_bar <- barplot(height = cluster_specific_hex_vals$prop,
+                      names = cluster_specific_hex_vals$classification,
+                      col = bar_colors,
                       main = paste0("freq (k=", charisma_obj$k, ", ", (charisma_obj$prop_threshold*100), "% thresh)"),
                       ylim = c(0,1), ylab = "Proportion of Image", las = 2)
   # text(freq_bar, round(color_summary$prop, 2) + .075, round(color_summary$prop, 2), cex = 1, srt = 90)
