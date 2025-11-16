de09bf180122f677e3ba00ad94d9297bd63100a9
Shawn Schwartz <shawn@shawnschwartz.com>
2025-11-15 23:46:38 -0800

    Improve documentation for clut data object

--- R/clut.data.R
+++ R/clut.data.R
@@ -4,6 +4,9 @@
 #' color space into 10 discrete color labels (i.e., black, white, grey, brown,
 #' red, orange, yellow, green, blue, and purple).
 #'
+#' @format A data frame with color boundary definitions for HSV color space.
+#'   Each row defines the HSV ranges for a specific discrete color category.
+#'
 #' @details
 #' These color boundaries were determined by forming consensus across three
 #' experts in the biology of color. Color boundaries were intentionally tuned
@@ -20,12 +23,12 @@
 #' stimulus. Leveraging contributions from the community will only help
 #' `charisma` be more useful for everybody who would like to use it!
 #'
-#' @docType data
+#' @seealso
+#' \code{\link{charisma}} for the main classification pipeline,
+#' \code{\link{validate}} for CLUT validation
 #'
-#' @usage charisma::clut
+#' @examples
+#' # View the default CLUT
+#' head(clut)
 #'
-#' @format An object of class \code{"data.frame"} to be passed into
-#' [charisma::charisma()] functions.
-#'
-#' @keywords charisma, color look up table, CLUT
 "clut"
