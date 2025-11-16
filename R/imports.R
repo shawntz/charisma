423355716a3d7a32055479dfd2546f9cf9f83f86
Shawn Schwartz <shawn@shawnschwartz.com>
2025-11-15 23:44:08 -0800

    Create imports.R

---
+++ R/imports.R
@@ -0,0 +1,11 @@
+#' @importFrom graphics abline axis barplot image mtext par rect
+#' @importFrom grDevices as.raster dev.off pdf png rgb rgb2hsv
+#' @importFrom stats aggregate setNames
+#' @importFrom utils combn
+#' @importFrom magrittr %>%
+#' @importFrom dplyr group_by mutate ungroup rowwise select transmute
+#' @importFrom tibble rownames_to_column as_tibble
+#' @importFrom recolorize readImage
+#' @importFrom purrr map
+#' @importFrom tidyr drop_na
+NULL
