33ab80e38fc434426234e6e3042d443291b72994
Shawn Schwartz <shawn@shawnschwartz.com>
2025-11-15 23:41:51 -0800

    Refactor and document plot.charisma and plotting functions

--- R/plot_charisma.R
+++ R/plot_charisma.R
@@ -1,37 +1,103 @@
-#' Add together two numbers
+#' Plot method for charisma objects
 #'
-#' @title this is a test
-#' @param x A number
-#' @param y A number
-#' @return The sum of \code{x} and \code{y}
+#' This function creates visualizations of color classification results from
+#' a charisma analysis. It can display the original image, recolored image,
+#' masked image, color proportions, and pavo color pattern geometry results.
+#'
+#' @param x A charisma object (output from \code{\link{charisma}} or
+#'   \code{\link{charisma2}}).
+#' @param plot.all Logical. If \code{TRUE}, plots all available visualizations.
+#'   Default is \code{TRUE}.
+#' @param plot.original Logical. If \code{TRUE}, plots the original image.
+#'   Default is \code{FALSE}.
+#' @param plot.recolored Logical. If \code{TRUE}, plots the recolored image
+#'   showing discrete color classifications. Default is \code{FALSE}.
+#' @param plot.masked Logical. If \code{TRUE}, plots the masked image after
+#'   background removal. Default is \code{FALSE}.
+#' @param plot.props Logical. If \code{TRUE}, plots a bar chart showing the
+#'   proportion of pixels in each color category. Default is \code{FALSE}.
+#' @param plot.pavo.img Logical. If \code{TRUE}, plots the image used for
+#'   \pkg{pavo} color pattern geometry analysis. Default is \code{FALSE}.
+#'   Only available if pavo analysis was performed.
+#' @param plot.pavo.classes Logical. If \code{TRUE}, plots the color palette
+#'   from \pkg{pavo} k-means clustering. Default is \code{FALSE}. Only available
+#'   if pavo analysis was performed.
+#' @param font.size Numeric. Size multiplier for plot text elements. Default
+#'   is \code{1.75}.
+#' @param props.x.cex Numeric. Size multiplier for x-axis labels in the
+#'   proportions plot. Default is \code{1.5}.
+#' @param real.bar.colors Logical. If \code{TRUE}, uses actual color values
+#'   for bars in the proportions plot. If \code{FALSE}, uses a default color
+#'   scheme. Default is \code{TRUE}.
+#' @param ... Additional arguments (currently not used).
+#'
+#' @return This function is called for its side effects (creating plots) and
+#'   does not return a value.
+#'
+#' @details
+#' When \code{plot.all = TRUE}, all available plots are displayed in a
+#' multi-panel layout. Individual plots can be selected by setting the
+#' corresponding \code{plot.*} parameters to \code{TRUE}.
+#'
+#' The function automatically detects whether \pkg{pavo} analysis results are
+#' present in the charisma object and adjusts the plot layout accordingly.
+#'
+#' @seealso
+#' \code{\link{charisma}} for the main classification pipeline,
+#' \code{\link{charisma2}} for batch processing
+#'
 #' @examples
-#' add(1, 1)
-#' add(10, 1)
+#' \dontrun{
+#' # Run charisma on an image
+#' result <- charisma("path/to/image.jpg")
+#'
+#' # Plot all results
+#' plot(result)
+#'
+#' # Plot only original and recolored images
+#' plot(result, plot.all = FALSE, plot.original = TRUE, plot.recolored = TRUE)
+#'
+#' # Plot color proportions
+#' plot(result, plot.all = FALSE, plot.props = TRUE)
+#' }
 #'
 #' @rdname plot.charisma
+#'
 #' @export
-plot.charisma <- function(x, plot.all = T, plot.original = F,
-                          plot.recolored = F, plot.masked = F,
-                          plot.props = F, plot.pavo.img = F,
-                          plot.pavo.classes = F, font.size = 1.75,
-                          props.x.cex = 1.5,
-                          real.bar.colors = T) {
+plot.charisma <- function(
+  x,
+  plot.all = TRUE,
+  plot.original = FALSE,
+  plot.recolored = FALSE,
+  plot.masked = FALSE,
+  plot.props = FALSE,
+  plot.pavo.img = FALSE,
+  plot.pavo.classes = FALSE,
+  font.size = 1.75,
+  props.x.cex = 1.5,
+  real.bar.colors = TRUE,
+  ...
+) {
   # reset graphical env
   current_par <- graphics::par(no.readonly = TRUE)
   on.exit(graphics::par(current_par))
 
   # set new graphical env defaults
-  par(cex.main = font.size, cex.lab = font.size,
-      cex.axis = font.size, mar = c(10, 1, 8, 1))
+  par(
+    cex.main = font.size,
+    cex.lab = font.size,
+    cex.axis = font.size,
+    mar = c(10, 1, 8, 1)
+  )
 
   # check whether charisma object contains pavo classifications
   has_pavo <- TRUE
 
   if (
     is.null(x$input2pavo) ||
-    is.null(x$pavo_adj_stats) ||
-    is.null(x$pavo_adj_class) ||
-    is.null(x$pavo_adj_class_plot_cols)
+      is.null(x$pavo_adj_stats) ||
+      is.null(x$pavo_adj_class) ||
+      is.null(x$pavo_adj_class_plot_cols)
   ) {
     has_pavo <- FALSE
     plot.pavo.img <- FALSE
@@ -48,22 +114,39 @@ plot.charisma <- function(x, plot.all = T, plot.original = F,
     if (has_pavo) {
       plot.pavo.img <- TRUE
       plot.pavo.classes <- TRUE
-      num_plots <- sum(c(plot.original, plot.recolored, plot.masked,
-                         plot.props, plot.pavo.img, plot.pavo.classes))
-      plot_layout <- matrix(c(0, 1, 4, 0, 0, 2, 5, 0, 0, 3, 6, 0),
-                            ncol = 3, nrow = 4)
+      num_plots <- sum(c(
+        plot.original,
+        plot.recolored,
+        plot.masked,
+        plot.props,
+        plot.pavo.img,
+        plot.pavo.classes
+      ))
+      plot_layout <- matrix(
+        c(0, 1, 4, 0, 0, 2, 5, 0, 0, 3, 6, 0),
+        ncol = 3,
+        nrow = 4
+      )
       plot_heights <- rep(c(0.15, 0.35, 0.35, 0.15), times = 3)
     } else {
-      num_plots <- sum(c(plot.original, plot.recolored, plot.masked,
-                         plot.props))
-      plot_layout <- matrix(c(0, 1, 3, 0, 0, 2, 4, 0),
-                            ncol = 2, nrow = 4)
+      num_plots <- sum(c(
+        plot.original,
+        plot.recolored,
+        plot.masked,
+        plot.props
+      ))
+      plot_layout <- matrix(c(0, 1, 3, 0, 0, 2, 4, 0), ncol = 2, nrow = 4)
       plot_heights <- rep(c(0.15, 0.35, 0.35, 0.15), times = 2)
     }
-
   } else {
-    num_plots <- sum(c(plot.original, plot.recolored, plot.masked,
-                       plot.props, plot.pavo.img, plot.pavo.classes))
+    num_plots <- sum(c(
+      plot.original,
+      plot.recolored,
+      plot.masked,
+      plot.props,
+      plot.pavo.img,
+      plot.pavo.classes
+    ))
     if (num_plots == 0) {
       stop("nothing to plot!")
     }
@@ -71,13 +154,15 @@ plot.charisma <- function(x, plot.all = T, plot.original = F,
     if (num_plots == 1) {
       plot_layout <- matrix(c(1), ncol = 1, nrow = 1)
       plot_heights <- 1
-    } else if (num_plots == 5 | num_plots == 6) {
-      plot_layout <- matrix(c(0, 1, 4, 0, 0, 2, 5, 0, 0, 3, 6, 0),
-                            ncol = 3, nrow = 4)
+    } else if (num_plots == 5 || num_plots == 6) {
+      plot_layout <- matrix(
+        c(0, 1, 4, 0, 0, 2, 5, 0, 0, 3, 6, 0),
+        ncol = 3,
+        nrow = 4
+      )
       plot_heights <- rep(c(0.15, 0.35, 0.35, 0.15), times = 3)
     } else if (num_plots == 4) {
-      plot_layout <- matrix(c(0, 1, 3, 0, 0, 2, 4, 0),
-                            ncol = 2, nrow = 4)
+      plot_layout <- matrix(c(0, 1, 3, 0, 0, 2, 4, 0), ncol = 2, nrow = 4)
       plot_heights <- rep(c(0.15, 0.35, 0.35, 0.15), times = 2)
     } else if (num_plots == 2) {
       plot_layout <- matrix(c(1, 2), ncol = 2, nrow = 1)
@@ -90,14 +175,17 @@ plot.charisma <- function(x, plot.all = T, plot.original = F,
 
   graphics::layout(plot_layout, heights = plot_heights)
 
-  # test layout
-  # layout.show(6)
+  if (plot.original) {
+    plot_original(x)
+  }
 
-  if (plot.original) plot_original(x)
+  if (plot.recolored) {
+    plot_recolored(x)
+  }
 
-  if (plot.recolored) plot_recolored(x)
-
-  if (plot.masked) plot_masked(x)
+  if (plot.masked) {
+    plot_masked(x)
+  }
 
   if (plot.props) {
     if (num_plots <= 4) {
@@ -108,7 +196,9 @@ plot.charisma <- function(x, plot.all = T, plot.original = F,
     plot_props(x, !real.bar.colors, mar, cex = props.x.cex)
   }
 
-  if (plot.pavo.img) plot_pavo_input(x)
+  if (plot.pavo.img) {
+    plot_pavo_input(x)
+  }
 
   if (plot.pavo.classes) {
     if (num_plots <= 4) {
--- R/plotting.R
+++ R/plotting.R
@@ -1,29 +1,57 @@
 plot_original <- function(charisma_obj, mar = c(0, 0, 5, 0)) {
   par(mar = mar)
   asp <- dim(charisma_obj$original_img)[1] / dim(charisma_obj$original_img)[2]
-  plot(0:1, 0:1, type = "n", axes = FALSE, asp = asp, main = "Original",
-       xlab = "", ylab = "")
+  plot(
+    0:1,
+    0:1,
+    type = "n",
+    axes = FALSE,
+    asp = asp,
+    main = "Original",
+    xlab = "",
+    ylab = ""
+  )
   graphics::rasterImage(charisma_obj$original_img, 0, 0, 1, 1)
 }
 
 plot_recolored <- function(img, mar = c(0, 0, 5, 0)) {
   par(mar = mar)
   asp <- dim(img$original_img)[1] / dim(img$original_img)[2]
-  plot(0:1, 0:1, type = "n", axes = FALSE, asp = asp, main = "Recolored",
-       xlab = "", ylab = "")
+  plot(
+    0:1,
+    0:1,
+    type = "n",
+    axes = FALSE,
+    asp = asp,
+    main = "Recolored",
+    xlab = "",
+    ylab = ""
+  )
   graphics::rasterImage(recolorize::recoloredImage(img), 0, 0, 1, 1)
 }
 
 plot_masked <- function(img, mar = c(0, 0, 5, 0)) {
   par(mar = mar)
   asp <- dim(img$original_img)[1] / dim(img$original_img)[2]
-  plot(0:1, 0:1, type = "n", axes = FALSE, asp = asp, main = "Charisma Mask",
-       xlab = "", ylab = "")
+  plot(
+    0:1,
+    0:1,
+    type = "n",
+    axes = FALSE,
+    asp = asp,
+    main = "Charisma Mask",
+    xlab = "",
+    ylab = ""
+  )
   graphics::rasterImage(img$color_mask, 0, 0, 1, 1)
 }
 
-plot_props <- function(charisma_obj, use.default.bar.colors = T, cex = 1.5,
-                       mar = c(5.5, 8, 5, 0)) {
+plot_props <- function(
+  charisma_obj,
+  use.default.bar.colors = TRUE,
+  cex = 1.5,
+  mar = c(5.5, 8, 5, 0)
+) {
   par(mar = mar)
 
   if (length(charisma_obj$dropped_colors) > 0) {
@@ -47,17 +75,26 @@ plot_props <- function(charisma_obj, use.default.bar.colors = T, cex = 1.5,
     dplyr::arrange(classification)
 
   if (use.default.bar.colors) {
-    bar_colors = cluster_specific_hex_vals$default.hex
+    bar_colors <- cluster_specific_hex_vals$default.hex
   } else {
-    bar_colors = cluster_specific_hex_vals$new.hex
+    bar_colors <- cluster_specific_hex_vals$new.hex
   }
 
-  freq_bar <- barplot(height = cluster_specific_hex_vals$prop,
-                      xaxt = "n",
-                      col = bar_colors,
-                      main = paste0("Color Profile (k = ", charisma_obj$k, ", ",
-                                    (charisma_obj$prop_threshold*100), "%)"),
-                      ylim = c(0,1), ylab = "Proportion of Image\n", las = 2)
+  freq_bar <- barplot(
+    height = cluster_specific_hex_vals$prop,
+    xaxt = "n",
+    col = bar_colors,
+    main = paste0(
+      "Color Profile (k = ",
+      charisma_obj$k,
+      ", ",
+      (charisma_obj$prop_threshold * 100),
+      "%)"
+    ),
+    ylim = c(0, 1),
+    ylab = "Proportion of Image\n",
+    las = 2
+  )
 
   labels <- cluster_specific_hex_vals$classification
   axis(1, at = freq_bar, tick = FALSE, labels = FALSE)
@@ -65,28 +102,68 @@ plot_props <- function(charisma_obj, use.default.bar.colors = T, cex = 1.5,
   for (i in 1:length(labels)) {
     if (!is.na(cluster_specific_hex_vals$prop[i])) {
       if (cluster_specific_hex_vals$prop[i] > charisma_obj$prop_threshold) {
-        mtext(labels[i], side = 1, at = freq_bar[i], line = 1,
-              cex = cex, col = "black", srt = 45, font = 2, las = 2) # bold
+        mtext(
+          labels[i],
+          side = 1,
+          at = freq_bar[i],
+          line = 1,
+          cex = cex,
+          col = "black",
+          srt = 45,
+          font = 2,
+          las = 2
+        ) # bold
       } else {
-        mtext(labels[i], side = 1, at = freq_bar[i], line = 1, cex = cex,
-              col = "gray50", srt = 45, font = 3, las = 2) # italicized
+        mtext(
+          labels[i],
+          side = 1,
+          at = freq_bar[i],
+          line = 1,
+          cex = cex,
+          col = "gray50",
+          srt = 45,
+          font = 3,
+          las = 2
+        ) # italicized
       }
     } else {
-      mtext(labels[i], side = 1, at = freq_bar[i], line = 1, cex = cex, col = "gray85", srt = 45, font = 1, las = 2)
+      mtext(
+        labels[i],
+        side = 1,
+        at = freq_bar[i],
+        line = 1,
+        cex = cex,
+        col = "gray85",
+        srt = 45,
+        font = 1,
+        las = 2
+      )
     }
   }
 
   if (charisma_obj$prop_threshold > 0) {
-    abline(h = charisma_obj$prop_threshold, col = "red",
-           lty = "dashed", lwd = 2)
+    abline(
+      h = charisma_obj$prop_threshold,
+      col = "red",
+      lty = "dashed",
+      lwd = 2
+    )
   }
 }
 
 plot_pavo_input <- function(charisma_obj, imgdat = NULL, mar = c(0, 0, 5, 0)) {
   par(mar = mar)
   asp <- dim(charisma_obj$original_img)[1] / dim(charisma_obj$original_img)[2]
-  plot(0:1, 0:1, type = "n", axes = FALSE, asp = asp, main = "Pavo",
-       xlab = "", ylab = "")
+  plot(
+    0:1,
+    0:1,
+    type = "n",
+    axes = FALSE,
+    asp = asp,
+    main = "Pavo",
+    xlab = "",
+    ylab = ""
+  )
   if (is.null(imgdat)) {
     graphics::rasterImage(charisma_obj$input2pavo, 0, 0, 1, 1)
   } else {
@@ -94,8 +171,12 @@ plot_pavo_input <- function(charisma_obj, imgdat = NULL, mar = c(0, 0, 5, 0)) {
   }
 }
 
-plot_pavo_pal <- function(charisma_obj, k = NULL, pal = NULL,
-                          mar = c(0, 5, 5, 5)) {
+plot_pavo_pal <- function(
+  charisma_obj,
+  k = NULL,
+  pal = NULL,
+  mar = c(0, 5, 5, 5)
+) {
   par(mar = mar)
   asp <- dim(charisma_obj$original_img)[1] / dim(charisma_obj$original_img)[2]
 
@@ -111,12 +192,16 @@ plot_pavo_pal <- function(charisma_obj, k = NULL, pal = NULL,
     k_col <- k
   }
 
-  image(seq_along(col_palette), 1, as.matrix(seq_along(col_palette)),
-        col = col_palette,
-        main = paste0("Pavo Classes (k = ", k_col, ")"),
-        xlab = "",
-        ylab = "",
-        xaxt = "n",
-        yaxt = "n",
-        asp = asp)
+  image(
+    seq_along(col_palette),
+    1,
+    as.matrix(seq_along(col_palette)),
+    col = col_palette,
+    main = paste0("Pavo Classes (k = ", k_col, ")"),
+    xlab = "",
+    ylab = "",
+    xaxt = "n",
+    yaxt = "n",
+    asp = asp
+  )
 }
